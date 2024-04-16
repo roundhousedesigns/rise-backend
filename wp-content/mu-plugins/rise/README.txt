366025cbf5457b0a851cc0cb8d0575dad2618e5a
Nick Gaswirth <nick@roundhouse-designs.com>
2024-04-15 20:27:45 -0400

    version bump v1.1.2

--- wp-content/mu-plugins/rise/README.txt
+++ wp-content/mu-plugins/rise/README.txt
@@ -21,6 +21,9 @@ Upload the `rise` directory to the `/wp-content/mu-plugins/` directory
 
 == Changelog ==
 
+= 1.1.2 =
+* Relate 'associate' and 'assistant' terms for more holistic searching.
+
 = 1.1.1 =
 * Minor update to function naming.
 * Finally started this changelog.
--- wp-content/mu-plugins/rise/rise.php
+++ wp-content/mu-plugins/rise/rise.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       Rise Theatre Directory
  * Description:       The main site functionality for the Rise Theatre Directory backend.
- * Version:           1.1.2beta
+ * Version:           1.1.2
  * Author:            Roundhouse Designs
  * Author URI:        https://roundhouse-designs.com
  * Text Domain:       rise
@@ -17,7 +17,7 @@ if ( !defined( 'WPINC' ) ) {
 /**
  * Current plugin version.
  */
-define( 'RISE_VERSION', '1.1.2beta' );
+define( 'RISE_VERSION', '1.1.2' );
 
 /**
  * The code that runs during plugin activation.
