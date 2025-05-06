6542ce00264f8b7e9679b104055ec6f174c25ffa
Nick Gaswirth <nick@roundhouse-designs.com>
2025-05-06 16:30:46 -0400

    no more submodules

---
+++ wp-content/themes/rhd-headless-theme/functions.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Theme functionality....such as it is.
+ *
+ * @package RHD
+ * @subpackage rhd-headless-theme
+ *
+ * @since 0.0.1
+ */
+
+/**
+ * Make sure we have thumbnails.
+ */
+add_theme_support('post-thumbnails');
