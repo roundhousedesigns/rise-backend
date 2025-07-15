9742ac95c97d4459b8ad93f66f87d95efd367022
Nick Gaswirth <nick@roundhouse-designs.com>
2025-07-15 16:17:35 -0400

    Enhance header structure and introduce social links functionality
    - Updated the header.php file to include social links template parts for better modularity.
    - Adjusted the layout and styling in CSS files to accommodate new mobile and desktop visibility classes.
    - Modified theme.json to update layout sizes and typography settings.
    - Added new SVG icons for social links in the template parts.
    - Improved button styles and focus states for better accessibility and user experience.

--- wp-content/themes/rise-theme/header.php
+++ wp-content/themes/rise-theme/header.php
@@ -1,8 +1,11 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html           <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<?php get_template_part( 'template-parts/svg', 'social-links' ); ?>
+
 	<?php wp_head(); ?>
 </head>
 
@@ -11,7 +14,7 @@
 	$page_color = 'bg-color-' . ( $pod->field( 'background_color' ) ? $pod->field( 'background_color' ) : 'bg-color-dark' );
 ?>
 
-<body <?php body_class( $page_color ); ?>>
+<body                     <?php body_class( $page_color ); ?>>
 	<?php wp_body_open(); ?>
 	<div id="page" class="site">
 		<header id="masthead" class="site-header">
@@ -29,21 +32,36 @@
 
 			<div class="spacer"></div>
 
-			<div class="directory-link wp-block-buttons">
+			<div class="desktop-only">
+				<?php get_template_part( 'template-parts/snippet', 'social-links' ); ?>
+			</div>
+
+			<div class="directory-link wp-block-buttons desktop-only">
 				<div class="wp-block-button">
-					<a href="<?php echo esc_url( home_url( '/directory' ) ); ?>" class="wp-block-button__link">Directory</a>
+					<a name="directory" href="<?php echo esc_url( home_url( '/directory' ) ); ?>" class="wp-block-button__link">Directory</a>
+				</div>
+				<div class="wp-block-button">
+					<a name="donate" href="<?php echo esc_url( home_url( '/donate' ) ); ?>" class="wp-block-button__link">Donate</a>
 				</div>
 			</div>
 
-			<button 
-				class="menu-toggle" 
-				aria-controls="primary-menu" 
+			<button
+				class="menu-toggle"
+				aria-controls="primary-menu"
 				aria-expanded="false"
 				aria-label="Toggle navigation menu">
 				<span class="hamburger-icon"></span>
 			</button>
 
 			<div class="menu-container">
+				<div class="mobile-nav-branding mobile-only">
+					<?php if ( has_custom_logo() ):
+							the_custom_logo();
+					endif; ?>
+
+					<?php get_template_part( 'template-parts/snippet', 'social-links' ); ?>
+				</div>
+
 				 <?php wp_nav_menu( [
 				 		'theme_location' => 'primary',
 				 		'menu_id'        => 'primary-menu',
---
+++ wp-content/themes/rise-theme/template-parts/snippet-social-links.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Template part for displaying social links
+ *
+ * @package Rise_Theme
+ * @subpackage Rise_Theme
+ * @since 1.0.0
+ */
+
+?>
+
+<div class="social-links">
+	<a href="https://facebook.com/risetheatreorg" class="icon social-link" target="_blank" rel="noopener noreferrer me">
+	<svg viewBox="23 23 64 64">
+			<use xlink:href="#facebook-unauth-icon" width="110" height="110"></use>
+		</svg>
+	</a>
+	<a href="https://www.instagram.com/risetheatreorg" class="icon social-link" target="_blank" rel="noopener noreferrer me">
+		<svg viewBox="23 23 64 64">
+			<use xlink:href="#instagram-unauth-icon" width="110" height="110"></use>
+		</svg>
+	</a>
+</div>
